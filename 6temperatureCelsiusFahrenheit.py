4b06fa421ad150ea21e5443ffce9d0c03cf85a9a
Bhupesh joshi <bhupesh.joshi054@gmail.com>
2026-04-26 17:20:45 +0530

    Add files via upload

---
+++ 6temperatureCelsiusFahrenheit.py
@@ -0,0 +1,14 @@
+# Read temperature in Celsius
+c = float(input())
+
+# Convert to Fahrenheit
+f = (c * 9/5) + 32
+
+# Print result
+print(f)
+
+#input
+#25
+
+#output
+#77.0
---
+++ 7fahrenheit to celsius.py
@@ -0,0 +1,18 @@
+# Read temperature in Fahrenheit
+f = float(input())
+
+# Convert to Celsius
+c = (f - 32) * 5/9
+
+# Print result
+print(c)
+
+#output
+
+Input:
+
+98.6
+
+Output:
+
+37.0
